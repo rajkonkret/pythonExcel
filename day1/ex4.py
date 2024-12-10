7ed9caefbc6c191974ae646b9f89be3ff5218b36
rajkonkret <rajkonkret660@gmail.com>
2024-12-10 10:51:49 +0100

    update ex4

--- day1/ex4.py
+++ day1/ex4.py
@@ -1,11 +1,9 @@
 import openpyxl
 
-from ex3 import total_sales
-
 wb = openpyxl.load_workbook('../data/videogamesales.xlsx')
 ws = wb.active
 
-ws = wb['vgsales']
+# ws = wb['vgsales']
 
 row_position = 1
 for i in range(1, ws.max_row):
@@ -18,4 +16,49 @@ for i in range(1, ws.max_row):
     total_sales = (NA_Sales + EU_Sales + JP_Sales + Other_Sales)
     ws.cell(row=row_position, column=11).value = total_sales
 
-wb.save('videogamesales.xlsx')  # save the updated workbook)
+new_row = (1, "The Legend of Zelda", 1986, "Action", "Nintendo", 3.74, 0.93, 1.69, 0.14, 6.51, 6.5)
+ws.append(new_row)
+
+# wb.save('videogamesales.xlsx')  # save the updated workbook)
+# wb.save('../data/videogamesales.xlsx')  # save the updated workbook)
+
+values = [ws.cell(row=ws.max_row, column=i).value for i in range(1, ws.max_column + 1)]
+print(values)
+# [1, 'The Legend of Zelda', 1986, 'Action', 'Nintendo', 3.74, 0.93, 1.69, 0.14, 6.51, 6.5]
+
+ws.delete_rows(ws.max_row, 1)
+# wb.save('videogamesales.xlsx')  # save the updated workbook)
+# wb.save('../data/videogamesales.xlsx')
+
+ws['P1'] = "Average Sales"
+ws['P2'] = '= AVERAGE(K2:K16220)'
+
+# wb.save('../data/videogamesales.xlsx')
+
+ws['Q1'] = "Number of Populated Cells"
+ws['Q2'] = "=COUNTA(E2:E16220)"
+
+# wb.save('../data/videogamesales.xlsx')
+
+ws['R1'] = "Number of Rows with Sports Genre"
+ws['R2'] = '=COUNTIF(E2:E16220, "Sports")'
+
+# wb.save('../data/videogamesales.xlsx')
+
+ws['S1'] = "Total sports Sales"
+ws['S2'] = '=SUMIF(E2:E16220, "Sports",K2:K16220)'
+
+ws['T1'] = "Rounded sum of Sports Sales"
+ws['T2'] = '=CEILING(S2,25)'
+
+print(ws.title)  # vgsales
+
+ws.title = "Video Games Sales Data"
+
+print(wb.sheetnames)
+
+wb.create_sheet('Empty Sheet ')
+print(wb.sheetnames)
+
+wb.save('../data/videogamesales.xlsx')
+
