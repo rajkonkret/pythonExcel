d783f867844157104b0dfe1837e863aabdfe4438
rajkonkret <rajkonkret660@gmail.com>
2024-12-10 15:10:23 +0100

    add ex12

---
+++ day1/ex12.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+height_data = [
+    {"Name": "Aditya", "Height": 179},
+    {"Name": "Sameer", "Height": 181},
+    {"Name": "Daarwin", "Height": 170},
+    {"Name": "Aditya", "Height": 167},
+]
+
+weight_data = [
+    {"Name": "Aditya", "Weight": 76},
+    {"Name": "Sameer", "Weight": 68},
+    {"Name": "Daarwin", "Weight": 69},
+    {"Name": "Aditya", "Weight": 73},
+]
+
+marks_data = [
+    {"Name": "Aditya", "Marks": 179},
+    {"Name": "Sameer", "Marks": 181},
+    {"Name": "Daarwin", "Marks": 170},
+    {"Name": "Aditya", "Marks": 167},
+]
+
+height_data_df = pd.DataFrame(height_data)
+weight_data_df = pd.DataFrame(weight_data)
+marks_data_df = pd.DataFrame(marks_data)
+writer = pd.ExcelWriter('excel_with_multiple_sheets.xlsx', engine='xlsxwriter')
+
+height_data_df.to_excel(writer, sheet_name='height', index=False)
+weight_data_df.to_excel(writer, sheet_name='weight', index=False)
+marks_data_df.to_excel(writer, sheet_name='marks', index=False)
+writer.close()
