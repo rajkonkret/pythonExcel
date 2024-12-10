111b027f824b60752f783da37b6566a9b78b8dd6
rajkonkret <rajkonkret660@gmail.com>
2024-12-10 14:51:41 +0100

    add ex10

---
+++ day1/ex10.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = [
+    {"Name": "Aditya", "Height": 179},
+    {"Name": "Sameer", "Height": 181},
+    {"Name": "Daarwin", "Height": 170},
+    {"Name": "Aditya", "Height": 167},
+]
+df = pd.DataFrame(data)
+print(df)
+
+writer = pd.ExcelWriter('excel_with_dict.xlsx', engine='xlsxwriter')
+df.to_excel(writer, sheet_name='first_sheet', index=False)
+writer.close()
